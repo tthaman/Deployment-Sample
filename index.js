7411356a3f26afc22bd92fa208412306da57c11d
Theresa Thaman <tthaman@gmail.com>
2020-08-24 19:28:36 -0700

    Added a Procfile.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const server = require("./server");
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost/deployment-sample', {
-  useNewUrlParser: true, 
-  useCreateIndex: true, 
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
   useUnifiedTopology: true
 }).then(() => {
   server.listen(port, () => {
